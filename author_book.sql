988d3c3dc99def093eef3aeeab17b552c67df760
Kakybat <82949206+kakybat@users.noreply.github.com>
2026-02-02 08:03:37 +0500

    3rd week python quiz

--- author_book.sql
+++ author_book.sql
@@ -22,8 +22,8 @@ VALUES ('1984', 'Dystopian', 1, 1949),
        ('Animal Farm', 'Satire', 1, 1945);
 
 
--- Link for TEST-2nd-week:
--- https://app.easyevaluate.com/attendee/test/Mj9Gkp0Yb5
+-- Link for TEST-3rd-week:
+-- https://app.easyevaluate.com/attendee/test/8JQcRa3VzA
 
 
 
